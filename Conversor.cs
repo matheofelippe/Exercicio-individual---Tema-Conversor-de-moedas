e46991647141d9b30e22a9d93bc20197933e765c
matheofelippe <69490109+matheofelippe@users.noreply.github.com>
2020-11-11 23:57:40 -0300

    Codigos

---
+++ Conversor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_individual___Tema_Conversor_de_moedas
+{
+    class conversor
+    {
+        // Declarar as variáveis
+        public static double cotacao;
+        public static double valor;
+        public static double iof = 1.06;
+
+        // Calcular valor em reais
+        public static double Reais()
+        {
+            return cotacao * (valor * iof);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercício_individual___Tema_Conversor_de_moedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é cotação do dólar? "); // Entrada do usuário para a cotação do dólar
+            conversor.cotacao = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos dólares irão ser comprados. "); // Entrada do usuário para a quantidade em dólar a ser comprada
+            conversor.valor = double.Parse(Console.ReadLine());
+
+            Console.Write("Valor em reais: " + conversor.Reais().ToString("F2", CultureInfo.InvariantCulture)); // Escrever valor em reais que ele ira pagar
+            // ao executar o código, tem que colocar vírgula ao invés de ponto
+
+            Console.ReadKey();
+        }
+    }
+}
